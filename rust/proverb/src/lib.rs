0a552777543975032b0da852272871690ad9e243
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2021-07-14 15:23:18 +0000

    proverb passes all tests

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut result = String::new();
+
+    for (index, &item) in list.iter().enumerate() {
+        if index == list.len() - 1 {
+            result.push_str(format!("And all for the want of a {}.", list[0]).as_str());
+        } else {
+            result.push_str(
+                format!("For want of a {} the {} was lost.\n", item, list[index + 1]).as_str(),
+            );
+        }
+    }
+    result
 }
